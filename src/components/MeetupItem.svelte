f859e4f0d2c4d30411986bb8e228d8070629adcc
Michał <sukowskimichal@gmail.com>
2021-11-07 22:55:13 +0100

    meetup component styling

---
+++ src/components/MeetupItem.svelte
@@ -0,0 +1,36 @@
+<div class="meetup">
+    <header>
+        <h1>Title</h1>
+    </header>
+    <p class="subtitle">Subtitle</p>
+    <div class="image">
+        <img src="/" alt="meetup" />
+    </div>
+    <p>description</p>
+    <p>adress</p>
+</div>
+
+<style>
+    .meetup {
+        width: 90%;
+        max-width: 1000px;
+        padding: 1.5rem;
+        background-color: rgb(248, 246, 242);
+        margin: 0 auto 3rem auto;
+    }
+
+    .meetup * {
+        text-align: center;
+    }
+
+    h1 {
+        text-align: center;
+        font: bold;
+        font-size: 40px;
+    }
+
+    .subtitle {
+        color: rgb(48, 45, 45);
+        font-size: 30px;
+    }
+</style>
