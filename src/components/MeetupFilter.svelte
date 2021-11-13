d26888edf024df341e8f1e9d6c6c335aba21ebab
Michał <sukowskimichal@gmail.com>
2021-11-13 16:09:16 +0100

    meetup filter component

---
+++ src/components/MeetupFilter.svelte
@@ -0,0 +1,35 @@
+<section class="meetups-control">
+    <button>All</button>
+    <button>Favorites</button>
+</section>
+
+<style>
+button {
+  background: #aaaaaa;
+  font: inherit;
+  font-size: 1rem;
+  cursor: pointer;
+  border: 1px solid #aaaaaa;
+  padding: 0.5rem 1rem;
+}
+
+button:focus {
+  outline: none;
+}
+
+button:first-of-type {
+  border-radius: 5px 0 0 5px;
+}
+
+button:last-of-type {
+  border-radius: 0 5px 5px 0;
+}
+
+button:hover,
+button:active,
+.active {
+  background: #cf0056;
+  border-color: #cf0056;
+  color: white;
+}
+</style>
